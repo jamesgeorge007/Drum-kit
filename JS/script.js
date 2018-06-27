db5f4b21d194184d2d1bab9b191dc71af96aff16
James George <jamesgeorge998001@gmail.com>
2018-06-27 21:27:39 +0530

    Tapped button is now raised a bit along with bunch of other workarounds.

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,16 @@
-let buttons = document.querySelectorAll('btn-group')
+let buttons = document.getElementsByTagName('button')
 let sounds = document.getElementsByTagName('audio')
-
+let size = sounds.length
+let index
 document.addEventListener('keydown', (event) => {
-  for(let audio of sounds){
-     if(audio.getAttribute('data-key') == event.keyCode){
-      audio.play()
+  for(i = 0; i < size; i++){
+     if(sounds[i].getAttribute('data-key') == event.keyCode){
+       buttons[i].classList = 'btn btn-tapped';
+       sounds[i].play()
+       index = i
     }
   }
+})
+document.addEventListener('keyup', () => {
+  buttons[index].classList = 'btn'
 })
